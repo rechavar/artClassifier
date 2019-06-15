7bfa1017c500ff89db1803749834dd03fb80fb34
rechavar <rechavar@eafit.edu.co>
2019-06-15 12:02:11 -0500

    data

--- data.py
+++ data.py
@@ -1,20 +1,30 @@
 import os
 import sys
-import cv2
 import json 
 import random
 import numpy as np
 import pandas as pd
+from collections import defaultdict
 
 def prepareDataset(dataDir):
     if not os.path.exists('image_files'):
         os.makedirs('image_files')
     
     annDir = os.path.join(dataDir, 'ann')
-    annList = [file for file in os.listdir(pascal_dir) if os.path.isfile(os.path.join(pascal_dir, file))]
+    annList = [file for file in os.listdir(annDir) if os.path.isfile(os.path.join(annDir, file))]
     metada = defaultdict(list)
     fn = lambda x: str(hash(x) % ((sys.maxsize + 1) * 2)) + '.PNG'
     for ann in annList:
-        jsonFile = json.dumps(ann)
-        datastore = json.loads(jsonFile)
-        
+        with open(os.path.join(annDir, ann),'r') as f:
+            dataStore = json.loads(f.read())
+        for i in dataStore['objects']:
+            newFileName = fn(ann)
+            metada['imageName'].append(newFileName)
+            metada['imageLabel'].append(i['tags'][0]['name'])
+
+def saveCropImage(filePath, newName, exterior):
+     
+
+
+
+prepareDataset(r'C:\Users\USER\Desktop\Universidad\Grupo de estudio DeepLearning\ART_DATASET\Dataset')
